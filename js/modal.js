dde9aecf612b0fdb5c7ee4bca7223a20163cc275
Jennifer Lingafelter <jlingafelter@gmail.com>
2018-07-24 22:07:10 -0400

    modal work

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,25 @@
-var $modalTrigger = $('.trigger');
-$modal = $('.modal');
-$modalClose = $('.modal__close');
-$modalActive = 'modal--active';
+// Get the modal
+var modal = document.getElementById('myModal');
 
-$modalTrigger.on('click', function(e) {
-  e.preventDefault();
-  console.log('hello');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-  $(this).siblings($modal).addClass($modalActive);
-$modal.attr('aria-hidden', 'false');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-});
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
-$modalClose.on('click', function(e) {
-  e.preventDefault();
-  console.log('Closing');
-  $(this).parents($modal).removeClass($modalActive);
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
