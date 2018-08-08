e67489ae6aea6dd89eab1fac3f3c6bfa32c5c6e0
Jennifer Lingafelter <jlingafelter@gmail.com>
2018-08-07 20:40:28 -0400

    Fixing plugins

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-//When you click on About Us, the font size and color will change and Grad Parties will appear after the heading
+//When you hover on the GRAD FOOD IS BEST section, it changes font color and background color and when you mouse out, it reverts back.
 
 $(document).ready(function(){
-  document.getElementById('hello').addEventListener("click", function(){
+  document.getElementById('hello').addEventListener("mouseover", function(){
     var hello=$('#hello');
     hello.css({
           'color': 'red',
+          'background-color': 'white',
    });
-   hello.after('GRad Parties');
+  });
+  document.getElementById('hello').addEventListener("mouseout", function(){
+    var hello=$('#hello');
+    hello.css({
+          'color': 'white',
+          'background-color': 'black',
+    });
   });
 });
